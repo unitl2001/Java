d7f479edf89330e0c76fc202712f9e2b0c417784
unitl2001 <unitl2001@10.37.129.2>
2018-07-26 20:30:04 +0800

    布林boolean

---
+++ BreadJava/src/tw/org/iii/mytest/Bread06.java
@@ -0,0 +1,14 @@
+package tw.org.iii.mytest;
+
+public class Bread06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b1 = true;
+		boolean b2 = false; 
+		int a = 10, b = 3;
+		boolean b3 = a > b;
+		System.out.println(b3);
+	}
+
+}
