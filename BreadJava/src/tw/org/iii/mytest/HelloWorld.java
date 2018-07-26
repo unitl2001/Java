7964e07a3129997d23588c45b44870bf670f87a8
unitl2001 <unitl2001@10.37.129.2>
2018-07-26 15:41:52 +0800

    Java初學

---
+++ BreadJava/src/tw/org/iii/mytest/HelloWorld.java
@@ -0,0 +1,13 @@
+package tw.org.iii.mytest;
+
+class HelloWorld {
+
+	public static void main(String[] args) { 
+		//紅色的是關鍵字為固定的 String 是字串陣列首字要大寫  args是變數
+		// TODO Auto-generated method stub  
+	System.out.println("Hello World");
+	//Syntax error, insert";" 如果有錯誤旁邊會有錯誤的提示
+	//命令列有三個串流 1.輸出 2.錯誤輸出 3.輸入
+	}
+
+}
---
+++ BreadJava/src/tw/org/iii/mytest/Ｈello2.java
@@ -0,0 +1,10 @@
+package tw.org.iii.mytest;
+
+public class Ｈello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub // 要找Task
+		System.out.println("Hello2");
+	}
+
+}
