27ad7dd3bc268b6e751e9a4f8d02addddd1a0fd3
unitl2001 <unitl2001@10.37.129.2>
2018-07-26 17:28:15 +0800

    int long short(範圍差異)

---
+++ BreadJava/src/tw/org/iii/mytest/Bread01.java
@@ -0,0 +1,45 @@
+package tw.org.iii.mytest;
+
+public class Bread01 {  //物件導向
+
+	public static void main(String[] args) { //語法寫這 該列別程式 主執行程式（主敘）
+		// TODO Auto-generated method stub 單列註解
+		/*
+		 * 	
+		 */ // /* * */ 區塊註解
+		// /**
+		// * javaDoc
+		// */
+		//一般寫程式會做變數的宣告
+		//java為強型別語言 任何變數一定要宣告才能使用 一但宣告完 給了之後不能變
+		//強型別一般用在編譯的動作稱為bytecode(位元組碼)
+		//弱型別語言 javascript php 給了值 可以改變（浮點數）
+		
+		////local variablae b1 稱為區域變數 function
+		byte b1; // (-1~-128)-128 ~127(0~127) java無正負號 => 256 => 2^8
+		byte b2 = 12; //對他宣告同時給值
+		byte b3 = -127; //128會編譯失敗 -129也會失敗
+		//變數命名原則[a-zA-Z$][a-zA-Z0-9$_]* 而且不要撞到關鍵字例如byte public
+		byte $_$ = 123;
+		//正規表示法Regular Expression 表示正規的規則
+		byte 變數1 = 123; //java支援unicode (但別這麼做)
+		System.out.println(變數1);
+		//預設編碼請設定為UTF-8 專案右鍵properties resource
+		
+		
+		//
+		short s1 = 128; //範圍2^16
+		short s2 = 32767;
+		//short s2 = 32768; 超過
+		
+		//平常使用int
+		int a1 = 123; // 範圍2^32 整數範圍有正負號
+		
+		long g1 = 123; //2^64
+		
+		
+		
+		
+	}
+
+}
---
+++ BreadJava/src/tw/org/iii/mytest/Bread02.java
@@ -0,0 +1,36 @@
+package tw.org.iii.mytest;
+
+public class Bread02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//簡單使用（玩）用int
+		int a1 = 10, a2 = 3;
+		//int a3 = a1 + a2
+		//int a3 = a1 - a2
+		//int a3 = a1 * a2
+		//int a3 = a1 / a2; //如果是除 其結果在a1/a2的運算過程是整數相除（此為強型別特性）
+		int a3 = a1 / 3 ;//可以
+		//int a3 = a1 / 3.3 這樣不行
+		System.out.println(a3);
+		
+		byte b1 = 10, b2 = 3;
+		//byte b3 = b1 / b2
+		//short b3 = b1 / b2 ; byte short 用數學運算 其運算結果為int
+		//int b3 = b1 / b2(正確)
+		byte b3 = (byte)(b1 / b2);
+		System.out.println(b3);
+		
+		byte b4 = 120 + 3;//(數字為常數,正確)
+		//byte b4 = b1 + 3; (b1為變數)
+		System.out.println(b4);
+		byte b5 = 127;
+		b5++; //++位元加1 所以會得出來的結果為-128
+		System.out.println(b5);
+		b5 --;
+		System.out.println(b5);
+		b5 += 2;
+		System.out.println(b5);
+	}
+
+}
